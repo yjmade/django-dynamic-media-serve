2926276e2d5e330b2ffe7df87f9f873cbc8200ca
spikeekips <spikeekips@gmail.com>
2008-04-16 16:46:45 +0000

    * add ETag for IEx. * add new settings, 	- HTTP_EXPIRE_INTERVAL: default expire header interval, set by seconds. 	- DEFAULT_IMAGE_RENDER_MODE: image re-rendering mode type. 	- DOJ_TMP_DIR: directory of temporary file.

--- dynamic_media_serve/trunk/__init__.py
+++ dynamic_media_serve/trunk/__init__.py
@@ -18,8 +18,8 @@
 """
 
 from django.http import Http404, HttpResponse, HttpResponseNotModified
-import mimetypes, time
-import os, zlib, sha, random
+import mimetypes, time, datetime
+import os, zlib, md5, random
 import rfc822
 import stat
 import urllib, urllib2
@@ -28,69 +28,122 @@ from django.utils.text import compress_string as django_compress_string
 from django.views import static as django_static
 from django.core.cache import cache
 from django.conf import settings
+from django.template import RequestContext
 
 if hasattr(settings, "CACHE_MIDDLEWARE_SECONDS") :
 	CACHE_MIDDLEWARE_SECONDS = settings.CACHE_MIDDLEWARE_SECONDS
 else :
 	CACHE_MIDDLEWARE_SECONDS = 10
 
-if hasattr(settings, "DEFAULT_MODE") :
-	DEFAULT_MODE = settings.DEFAULT_MODE
+if hasattr(settings, "DOJ_TMP_DIR") :
+	DOJ_TMP_DIR = settings.DOJ_TMP_DIR
 else :
-	DEFAULT_MODE = "ratio"
+	DOJ_TMP_DIR = "/tmp/doj/"
+	if not os.path.exists(DOJ_TMP_DIR) :
+		os.makedirs(DOJ_TMP_DIR)
 
-try :
-	from jsmin import jsmin
-except :
-	def jsmin (s) : return s
+if hasattr(settings, "HTTP_EXPIRE_INTERVAL") :
+	HTTP_EXPIRE_INTERVAL = settings.HTTP_EXPIRE_INTERVAL
+else :
+	HTTP_EXPIRE_INTERVAL = 10 * 24 * 60 * 60 # 10 days
+
+if hasattr(settings, "DEFAULT_IMAGE_RENDER_MODE") :
+	DEFAULT_IMAGE_RENDER_MODE = settings.DEFAULT_IMAGE_RENDER_MODE
+else :
+	DEFAULT_IMAGE_RENDER_MODE = "ratio"
 
 import image
 
-def serve(request, path, document_root=None, show_indexes=False):
+def serve (request, path, document_root=None, show_indexes=False) :
 	__argument = request.GET.copy()
-	__use_cache = not __argument.has_key("update")
 
-	__compress = __argument.get("compress", "").lower()
-	if __compress not in ("gzip", "deflate", ) :
-		__compress = None
+	force_mimetype = __argument.get("force_mimetype", "").strip()
+	use_cache = not __argument.has_key("update")
+	if __argument.get("compress", "").lower() not in ("gzip", "deflate", ) :
+		compress = None
 	else :
-		if True not in [i == __compress for i in request.META.get("HTTP_ACCEPT_ENCODING", "").split(",")] :
-			__compress = None
+		if True not in [i == compress for i in request.META.get("HTTP_ACCEPT_ENCODING", "").split(",") if i.strip()] :
+			compress = None
 
-	if path.startswith("http%3A%2F%2F") :
+	if path.startswith("http%3A%2F%2F") or path.startswith("http://") :
 		fullpath =  urllib.unquote(path)
 		func_get_media = get_media_external
 	else :
 		fullpath = os.path.join(document_root, path)
-		if os.path.isdir(fullpath):
-			raise Http404, "Directory indexes are not allowed here."
-
-		if not os.path.exists(fullpath):
+		if not os.path.exists(fullpath) :
 			raise Http404, "'%s' does not exist" % fullpath
+
+		if os.path.isdir(fullpath) : # DOJ does not support directory index page.
+			raise Http404, "Directory indexes are not allowed here."
 
 		func_get_media = get_media_internal
 
-	(contents, mimetype, status_code, last_modified, response, ) = func_get_media(
-		request, fullpath, use_cache=__use_cache)
-
-	## Respect the If-Modified-Since header.
-	if status_code == 304 :
+	# for IE series, check 'HTTP_IF_NONE_MATCH' first.
+	if not use_cache and \
+			request.META.get("HTTP_IF_NONE_MATCH", None) == get_etag(fullpath) :
 		return HttpResponseNotModified()
+	elif not use_cache and not was_modified_since(request, fullpath) :
+		return HttpResponseNotModified()
+	else :
+		if use_cache :
+			# We use cache. If you did not enable the caching,
+			# nothing will be happended.
+			response = cache.get(get_cache_name(request, path))
+			if response :
+				return response
 
-	if not response :
-		response = HttpResponse(
-			__compress and compress_string(contents, __compress) or contents,
-			mimetype=mimetype
-		)
-		response["Last-Modified"] = last_modified
+	(contents, mimetype, status_code, last_modified, ) = func_get_media( \
+		request,
+		fullpath,
+		use_cache=use_cache,
+	)
 
-		if __compress :
-			response["Content-Encoding"] = __compress
+	response = HttpResponse(
+		compress and compress_string(contents, compress) or contents,
+		mimetype=force_mimetype and force_mimetype or mimetype
+	)
+	response["Last-Modified"] = last_modified
+	response["Expires"] = rfc822.formatdate(time.mktime((datetime.datetime.now() + datetime.timedelta(seconds=HTTP_EXPIRE_INTERVAL)).timetuple()))
+	response["ETag"] = get_etag(fullpath)
 
-		__path_cache = urllib.quote("%s?%s" % (fullpath, __argument.urlencode()), "")
-		cache.set(__path_cache, response, CACHE_MIDDLEWARE_SECONDS)
+	if compress :
+		response["Content-Encoding"] = compress
+
+	cache.set(
+		get_cache_name(request, fullpath),
+		response, CACHE_MIDDLEWARE_SECONDS,
+	)
 
 	return response
+
+def was_modified_since (request, path) :
+	statobj = os.stat(path)
+	return django_static.was_modified_since(
+		request.META.get("HTTP_IF_MODIFIED_SINCE", None),
+		statobj[stat.ST_MTIME],
+		statobj[stat.ST_SIZE]
+	)
+
+def get_mime_handler (mimetype) :
+	__media_type = "func_%s" % mimetype.replace("/", "_").replace("-", "__")
+	if globals().has_key(__media_type) :
+		fn = globals().get(__media_type)
+	else :
+		__media_type = mimetype.split("/")[0]
+		fn = globals().get("func_%s" % __media_type, func_default)
+
+	return fn
+
+def get_cache_name (request, path) :
+	return urllib.quote("%s?%s" % (path, request.GET.urlencode()), "")
+
+def get_etag (path) :
+	try :
+		mtime = os.stat(path)[stat.ST_MTIME]
+	except :
+		mtime = time.time()
+
+	return urllib.quote(path + str(mtime), "")
 
 def compress_string (s, mode="gzip") :
 	if mode == "gzip" :
@@ -99,11 +152,29 @@ def compress_string (s, mode="gzip") :
 		return zlib.compress(s)
 	else :
 		return s
+from django.template import Template
+def get_rendered_to_string (request, content) :
+	if not request.GET.has_key("use_template") :
+		return content
+	else :
+		try :
+			t = Template(content)
+			return t.render(RequestContext(request))
+		except Exception, e :
+			if settings.DEBUG :
+				print e
 
-def func_image (request, path) :
+			return content
+
+##################################################
+# Mimetype handler
+def func_image (request, fd, path=None) :
+	if path is None :
+		return fd.read()
+
 	__argument = request.GET.copy()
 
-	__mode = __argument.get("mode", )
+	__mode = __argument.get("mode", DEFAULT_IMAGE_RENDER_MODE, )
 	__direction = __argument.get("direction", "center")
 	__update = __argument.has_key("update")
 
@@ -133,18 +204,21 @@ def func_image (request, path) :
 			print "[EE]" , e
 			pass
 
-	return open(path, "rb").read()
+	return fd.read()
 
-def func_application_x__javascript (request, path) :
-	return jsmin(file(path).read())
+def func_application_x__javascript (request, fd, path=None) :
+	return get_rendered_to_string(request, fd.read())
 
-def func_text_html (request, path) :
-	return open(path, "rb").read()
+def func_text_html (request, fd, path=None) :
+	return get_rendered_to_string(request, fd.read())
 
-def func_default (request, path) :
-	return open(path, "rb").read()
+def func_text_css (request, fd, path=None) :
+	return get_rendered_to_string(request, fd.read())
 
-def get_media_external (request, path, use_cache=True) :
+def func_default (request, fd, path=None) :
+	return fd.read()
+
+def get_media_external (request, path, use_cache=True, ) :
 	req = urllib2.Request(path)
 	if request.META.get("HTTP_REFERER", None) :
 		req.add_header("Referer", request.META.get("HTTP_REFERER"))
@@ -155,71 +229,52 @@ def get_media_external (request, path, use_cache=True) :
 			request.META.get("HTTP_IF_MODIFIED_SINCE")
 		)
 
+	if request.META.get("HTTP_IF_NONE_MATCH", None) :
+		req.add_header(
+			"If-None-Match",
+			request.META.get("HTTP_IF_NONE_MATCH")
+		)
+
 	try :
 		r = urllib2.urlopen(req)
 	except urllib2.HTTPError, e :
 		(contents, mimetype, status_code, last_modified, ) = (
-			None, None, e.code, e.headers.getheader("last-modified"), )
+			"", None, e.code, e.headers.getheader("last-modified"), )
 	else :
 		mimetype = r.headers.getheader("content-type")
+
 		# save in tmp
-		path = "/tmp/%s%s" % (
-				sha.new(str(random.random())).hexdigest(),
-				mimetypes.guess_extension(mimetype)
+		try :
+			path = "%s/%s%s" % (
+				DOJ_TMP_DIR,
+				md5.new(str(random.random())).hexdigest(),
+				mimetypes.guess_extension(mimetype),
 			)
-		tmp = file(path, "w")
-		tmp.write(r.read())
-		tmp.close()
+			tmp = file(path, "w")
+			tmp.write(r.read())
+			tmp.close()
+		except :
+			return (r.read(), mimetype, status_code, last_modified, )
 
 		last_modified = r.headers.getheader("last-modified")
 		status_code = 200
 
-		__media_type = "func_%s" % mimetype.replace("/", "_").replace("-", "__")
-		if globals().has_key(__media_type) :
-			fn = globals().get(__media_type)
-		else :
-			__media_type = mimetype.split("/")[0]
-			fn = globals().get("func_%s" % __media_type, func_default)
-
-		contents = fn(request, path)
+		contents = get_mime_handler(mimetype)(request, file(path, "rb"), path=path)
 		os.remove(path)
 
-	return (contents, mimetype, status_code, last_modified, None, )
+	return (contents, mimetype, status_code, last_modified, )
 
 def get_media_internal (request, path, use_cache=True) :
-	statobj = os.stat(path)
-	(st_mtime, st_size, ) = (statobj[stat.ST_MTIME], statobj[stat.ST_SIZE], )
+	# get media type
+	mimetype = mimetypes.guess_type(path)[0]
+	contents = get_mime_handler(mimetype)(request, file(path, "rb"), path=path)
 
-	if not django_static.was_modified_since(
-			request.META.get("HTTP_IF_MODIFIED_SINCE", None), st_mtime, st_size) :
-		status_code = 304
-		mimetype = None
-		contents = None
-	else :
-		status_code = 200
-
-		if use_cache :
-			__argument = request.GET.copy()
-
-			# We use cache. If you did not enable the caching, nothing will be happended.
-			__path_cache = urllib.quote("%s?%s" % (path, __argument.urlencode()), "")
-			__response = cache.get(__path_cache)
-			if __response :
-				return (None, None, status_code, None, __response, )
-
-		# get media type
-		mimetype = mimetypes.guess_type(path)[0]
-
-		__media_type = "func_%s" % mimetype.replace("/", "_").replace("-", "__")
-		if globals().has_key(__media_type) :
-			fn = globals().get(__media_type)
-		else :
-			__media_type = mimetype.split("/")[0]
-			fn = globals().get("func_%s" % __media_type, func_default)
-
-		contents = fn(request, path)
-
-	return (contents, mimetype, status_code, rfc822.formatdate(st_mtime), None, )
+	return (
+		contents,
+		mimetype,
+		200,
+		rfc822.formatdate(os.stat(path)[stat.ST_MTIME]),
+	)
 
 
 """
--- dynamic_media_serve/trunk/image.py
+++ dynamic_media_serve/trunk/image.py
@@ -17,149 +17,155 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 
-import urllib, Image, os
+import urllib, os
 
-def get_image_offset_sooa (size_orig, size_new, direction="center") :
-	(w, h, ) = size_orig
-	(w_new, h_new, ) = size_new
+try :
+	import Image
+except :
+	def resize_image (*args, **kwargs) :
+		return open(args[0], "rb").read()
+else :
+	def get_image_offset_sooa (size_orig, size_new, direction="center") :
+		(w, h, ) = size_orig
+		(w_new, h_new, ) = size_new
 
-	# get image size ratio
-	__pos = (0, 0, w_new, h_new, )
+		# get image size ratio
+		__pos = (0, 0, w_new, h_new, )
 
-	if w > h : # resize height.
-		__ratio = float(h_new) / float(h)
-		h = h_new
-		w = w * __ratio
-
-		if direction == "topleft" :
-			__offset_x0 = 0
-			__offset_x1 = w_new
-		else :
-			if w > w_new :
-				__offset_x0 = int(float(w - w_new) / float(2))
-				__offset_x1 = int(w - float(w - w_new) / float(2))
-			else :
-				__offset_x0 = 0
-				__offset_x1 = int(w)
-
-		__pos = (
-			__offset_x0,
-			0,
-			__offset_x1,
-			int(h),
-		)
-	elif h > w : # resize width
-		__ratio = float(w_new) / float(w)
-		w = w_new
-		h = h * __ratio
-
-		if direction == "topleft" :
-			__offset_y0 = 0
-			__offset_y1 = h_new
-		else :
-			if h > h_new :
-				__offset_y0 = int(float(h - h_new) / float(2))
-				__offset_y1 = int(h - float(h - h_new) / float(2))
-			else :
-				__offset_y0 = 0
-				__offset_y1 = h
-
-		__pos = (
-			0,
-			__offset_y0,
-			int(w),
-			__offset_y1,
-		)
-	else : # height == width
-		__offset_x0 = (w / 2) - (w_new / 2)
-		__offset_x1 = __offset_x0 + w_new
-
-		__offset_y0 = (h / 2) - (h_new / 2)
-		__offset_y1 = __offset_y0 + h_new
-
-		__pos = (
-			__offset_x0,
-			__offset_y0,
-			__offset_x1,
-			__offset_y1,
-		)
-
-	return ((int(w), int(h), ), __pos, )
-
-def resize_image (path, size=(200, 200), mode="ratio", direction="center") :
-	im = Image.open(path)
-
-	(w, h, ) = im.size
-
-	w_new = size[0]
-	h_new = size[1]
-
-	if w_new > w and h_new > h :
-		return open(path, "rb").read()
-
-	if not w_new or not h_new :
-		mode="ratio"
-
-	#if (w_new is not None or h_new is not None) and mode == "sooa" :
-
-	if (w_new is not None or h_new is not None) and mode == "sooa" :
-		((w, h, ), __pos, ) = get_image_offset_sooa(im.size, size, direction, )
-		im = im.resize((w, h, ), Image.ANTIALIAS)
-		im = im.crop(__pos)
-	elif (w_new is not None or h_new is not None) and mode == "flickr_center" :
-		if h_new and h > h_new :
+		if w > h : # resize height.
 			__ratio = float(h_new) / float(h)
 			h = h_new
 			w = w * __ratio
 
-		# resizing
-		im = im.resize((int(w), int(h), ), Image.ANTIALIAS)
+			if direction == "topleft" :
+				__offset_x0 = 0
+				__offset_x1 = w_new
+			else :
+				if w > w_new :
+					__offset_x0 = int(float(w - w_new) / float(2))
+					__offset_x1 = int(w - float(w - w_new) / float(2))
+				else :
+					__offset_x0 = 0
+					__offset_x1 = int(w)
 
-		# center-focused crop
-		if w > w_new :
-			im = im.crop(
-				(
-					int(float(w - w_new) / float(2)),
-					0,
-					int(w - (float(w - w_new) / float(2))),
-					h,
-				)
-			)
-	elif (w_new is not None or h_new is not None) and mode == "flickr" :
-		im = im.crop(
-			(
+			__pos = (
+				__offset_x0,
 				0,
-				0,
-				(w_new < w) and w_new or w,
-				(h_new < h) and h_new or h,
+				__offset_x1,
+				int(h),
 			)
-		)
-	else :
-		if w_new and w > w_new :
+		elif h > w : # resize width
 			__ratio = float(w_new) / float(w)
 			w = w_new
 			h = h * __ratio
 
-		if h_new and h > h_new :
-			__ratio = float(h_new) / float(h)
-			h = h_new
-			w = w * __ratio
+			if direction == "topleft" :
+				__offset_y0 = 0
+				__offset_y1 = h_new
+			else :
+				if h > h_new :
+					__offset_y0 = int(float(h - h_new) / float(2))
+					__offset_y1 = int(h - float(h - h_new) / float(2))
+				else :
+					__offset_y0 = 0
+					__offset_y1 = h
 
-		# resizing
-		im = im.resize((int(w), int(h), ), Image.ANTIALIAS)
+			__pos = (
+				0,
+				__offset_y0,
+				int(w),
+				__offset_y1,
+			)
+		else : # height == width
+			__offset_x0 = (w / 2) - (w_new / 2)
+			__offset_x1 = __offset_x0 + w_new
 
-	__path = os.path.join("/tmp", urllib.quote(path, ""))
-	try :
-		im.save(__path, **im.info)
-	except KeyError, e :
-		__path += ".jpg"
+			__offset_y0 = (h / 2) - (h_new / 2)
+			__offset_y1 = __offset_y0 + h_new
 
-		im.save(__path, **im.info)
+			__pos = (
+				__offset_x0,
+				__offset_y0,
+				__offset_x1,
+				__offset_y1,
+			)
 
-	contents = open(__path, "rb").read()
-	os.remove(__path)
+		return ((int(w), int(h), ), __pos, )
 
-	return contents
+	def resize_image (path, size=(200, 200), mode="ratio", direction="center") :
+		im = Image.open(path)
+
+		(w, h, ) = im.size
+
+		w_new = size[0]
+		h_new = size[1]
+
+		if w_new > w and h_new > h :
+			return open(path, "rb").read()
+
+		if not w_new or not h_new :
+			mode="ratio"
+
+		#if (w_new is not None or h_new is not None) and mode == "sooa" :
+
+		if (w_new is not None or h_new is not None) and mode == "sooa" :
+			((w, h, ), __pos, ) = get_image_offset_sooa(im.size, size, direction, )
+			im = im.resize((w, h, ), Image.ANTIALIAS)
+			im = im.crop(__pos)
+		elif (w_new is not None or h_new is not None) and mode == "flickr_center" :
+			if h_new and h > h_new :
+				__ratio = float(h_new) / float(h)
+				h = h_new
+				w = w * __ratio
+
+			# resizing
+			im = im.resize((int(w), int(h), ), Image.ANTIALIAS)
+
+			# center-focused crop
+			if w > w_new :
+				im = im.crop(
+					(
+						int(float(w - w_new) / float(2)),
+						0,
+						int(w - (float(w - w_new) / float(2))),
+						h,
+					)
+				)
+		elif (w_new is not None or h_new is not None) and mode == "flickr" :
+			im = im.crop(
+				(
+					0,
+					0,
+					(w_new < w) and w_new or w,
+					(h_new < h) and h_new or h,
+				)
+			)
+		else :
+			if w_new and w > w_new :
+				__ratio = float(w_new) / float(w)
+				w = w_new
+				h = h * __ratio
+
+			if h_new and h > h_new :
+				__ratio = float(h_new) / float(h)
+				h = h_new
+				w = w * __ratio
+
+			# resizing
+			im = im.resize((int(w), int(h), ), Image.ANTIALIAS)
+
+		__path = os.path.join("/tmp", urllib.quote(path, ""))
+		try :
+			im.save(__path, **im.info)
+		except KeyError, e :
+			__path += ".jpg"
+
+			im.save(__path, **im.info)
+
+		contents = open(__path, "rb").read()
+		os.remove(__path)
+
+		return contents
 
 
 
